4ee7094ce97dbf9feb94cd5f8122158d6bf8c477
rubens.junior@outlook.com.br <rubens.junior@outlook.com.br>
2022-07-14 21:11:16 -0300

    inserindo outros components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import Header from "./components/Header";
 import NotificationButton from "./components/notificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
-  return(
+  return (
     <>
-    <h1>Ola Mundo</h1>
-    <NotificationButton/>
+
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
